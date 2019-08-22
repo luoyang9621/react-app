185cddbaa29dd94daa20819bd3c6c2a0d51e9e9b
ly9621 <962136682@qq.com>
2019-08-22 19:06:58 +0800

    '准备按照现有的框架去做一个简单的demo页面'

--- src/index.js
+++ src/index.js
@@ -1,8 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'lib-flexible';
+import './static/css/base.css';
+import './static/css/common.css';
 import './index.css';
-import BaseRouter from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<BaseRouter />, document.getElementById('root'));
-registerServiceWorker();
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div className='login-header'>
+                <div className="avatar">
+                    <img src='{this.props.avatar}' alt=""/>
+                </div>
+                <p>请先登陆</p>
+            </div>
+        )
+    }
+}
+
+class Login extends React.Component {
+ render() {
+     return (
+         <div className='login-wrap'>
+             <Header />
+         </div>
+     )
+ }
+}
+
+
+
+ReactDOM.render(
+    <Login/>,
+    document.getElementById('root')
+);
